267ef3d9f556d0d2235d036218a5246807b3320b
Payas Parab <pparab19@berkeley.edu>
2019-01-12 11:26:34 -0800

    removed all the files for twilio project

--- key_imports.py
+++
@@ -1,7 +0,0 @@
-import json
-key_file = 'authentication.json'
-# Loading your keys from keys.json (which you should have filled
-# in in question 1):
-with open(key_file) as f:
-    keys = json.load(f)
-# if you print or view the contents of keys be sure to delete the cell!
