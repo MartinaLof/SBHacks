f813c914d8e6035e8773e6a1b85f936b7947a61e
Martina Lof <martinalofqvist@outlook.com>
2019-01-12 15:47:15 -0800

    Routing

--- OptiClass/src/app/app.module.ts
+++ OptiClass/src/app/app.module.ts
@@ -5,16 +5,28 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ResultsComponent } from './results/results.component';
+
+// Angular Material
+import {
+  MatButtonModule,
+  MatCheckboxModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ResultsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
